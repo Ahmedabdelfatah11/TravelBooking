355c2979ca374b25b8ba845cdf6f0ceb9b6f64d5
Abanoubemad21 <Abanoubemad21>
2025-08-03 23:08:53 +0300

    Add JSON data files

--- TravelBooking.Service/obj/Debug/net9.0/TravelBooking.Service.AssemblyInfo.cs
+++ TravelBooking.Service/obj/Debug/net9.0/TravelBooking.Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TravelBooking.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eff9ff656ea8494f95d1b38c3ac32e262150e004")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da50805ffc96756a1357e64f83568dc926e0cfbd")]
 [assembly: System.Reflection.AssemblyProductAttribute("TravelBooking.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TravelBooking.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
