35a4ec6f486a5034bcb389833a541186ef2381a1
Ayman Abd_Elnaby Mohamed <aa5183@fayoum.edu.eg>
2025-08-10 19:08:19 +0300

    - editing in Payment

--- TravelBooking.Repository/Migrations/20250810144857_v1.cs
+++ TravelBooking.Repository/Migrations/20250810144857_v1.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace TravelBooking.Repository.Migrations
 {
     /// <inheritdoc />
-    public partial class initialversion : Migration
+    public partial class v1 : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
